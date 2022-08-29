4d5dccd32ee4e920751f08722db8313712d0ac16
Roman <orientir116@gmail.com>
2022-08-30 01:04:35 +0300

    save title and state

--- src/components/todo-item.vue
+++ src/components/todo-item.vue
@@ -4,10 +4,26 @@
       <input
         class="todo-item__checkbox"
         type="checkbox"
+        :id="task._id"
         v-model="task.done"
-        @click.stop
+        @click.stop="editStatus(task._id)"
       />
-      <span class="todo-item__title">{{ task.title }}</span>
+      <label :for="task._id"></label>
+      <span
+        v-if="!editCurrentItem"
+        :class="['todo-item__title', { done: task.done }]"
+        @click="editItemTitle"
+        >{{ task.title }}</span
+      >
+      <input
+        v-model="currentItemTitle"
+        v-else
+        ref="input"
+        type="text"
+        @blur="editCurrentItem = false"
+        @keyup.esc="editCurrentItem = false"
+        @keyup.enter="editTitle"
+      />
     </div>
     <div class="todo-item__action" @click.stop="$emit('remove')">
       <svg
@@ -27,10 +43,36 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps } from "vue";
-defineProps<{
+import { defineProps, ref, nextTick } from "vue";
+import { TodoStore } from "../stores/store";
+const store = TodoStore();
+
+const props = defineProps<{
   task: any;
 }>();
+
+const editCurrentItem = ref<boolean>(false);
+const currentItemTitle = ref<string>("");
+
+const input = ref<any>(null);
+async function editItemTitle() {
+  editCurrentItem.value = true;
+  await nextTick();
+  input.value.focus();
+  currentItemTitle.value = props.task.title;
+}
+async function editTitle() {
+  const item = await store.updateTodo(props.task._id, {
+    title: currentItemTitle.value,
+  });
+  props.task.title = currentItemTitle.value;
+  editCurrentItem.value = false;
+}
+async function editStatus(id: string) {
+  const item = await store.updateTodo(id, {
+    done: !props.task.done,
+  });
+}
 </script>
 
 <style lang="scss">
@@ -54,10 +96,52 @@ defineProps<{
   &__inner {
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    width: 100%;
+    label {
+      padding-right: 20px;
+    }
+    .done {
+      text-decoration-line: line-through;
+      color: #9799ad;
+    }
+    input[type="text"] {
+      border: none;
+      background: none;
+      width: 100%;
+      outline: none;
+      font-size: 16px;
+      color: #fafafd;
+    }
   }
   &__checkbox {
-    margin-right: 20px;
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+  }
+  &__checkbox + label {
+    display: inline-flex;
+    align-items: center;
+    user-select: none;
+    cursor: pointer;
+  }
+  &__checkbox + label::before {
+    content: "";
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    flex-shrink: 0;
+    flex-grow: 0;
+    // box-shadow: 0 0 0 2px #67697a;
+    border: 2px solid #67697a;
+    border-radius: 50%;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: 50% 50%;
+  }
+  &__checkbox:checked + label::before {
+    border-color: #4ea2ff;
+    background-color: #4ea2ff;
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%222435' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
   }
 
   &__title {
